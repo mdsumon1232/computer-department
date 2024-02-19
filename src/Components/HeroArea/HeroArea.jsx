dbb79cc673720f8ae0b3f4f414049be3f565ca3f
Md.Sumon <sumonahamed5566@gmail.com>
2024-02-19 15:07:41 +0600

    add hero area

--- src/Components/HeroArea/HeroArea.jsx
+++ src/Components/HeroArea/HeroArea.jsx
@@ -1,13 +1,19 @@
+import heroImg from "../../assets/heroImage.png";
 import "./HeroArea.css";
 
 const HeroArea = () => {
   return (
     <div className="hero-container">
-      <div className="textContainer">
-        <h1> Welcome to Computer Department</h1>
-        <p>Tangail Polytechnic Institute</p>
+      <div className="text">
+        <div className="hero-text">
+          <h1>Welcome to Computer Department</h1>
+          <h2>Tangail polytechnic Institute</h2>
+        </div>
       </div>
-      <div className="image"></div>
+      <div className="heroImage">
+        <img src={heroImg} alt="" />
+      </div>
+      <div className="blank"> </div>
     </div>
   );
 };
